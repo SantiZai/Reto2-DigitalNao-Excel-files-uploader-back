3c886fe4d371e4913781bb91d87a47c0dfacd792
SantiZai <santyagozaidan@gmail.com>
2024-01-11 23:04:22 -0300

    login functionality

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -27,3 +27,22 @@ export const saveUser = async (req, res) => {
       .json({ message: "Ha ocurrido un error al guardar los datos" });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const existingUser = await User.findOne({ username });
+    !existingUser && res.status(404).json({ message: "User not found" })
+    const isValidPassword = compare(password, existingUser.password);
+    // TODO: add jwt to return to the front
+    isValidPassword
+      ? res.status(200).json({ message: "Logged in" })
+      : res.status(404).json({ message: "The password are invalid" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message:
+        "Ha ocurrido un error al tratar de recuperar los datos de la cuenta",
+    });
+  }
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getUsers, saveUser } from "../controllers/userControllers.js";
+import { getUsers, login, saveUser } from "../controllers/userControllers.js";
 
 const router = Router();
 
 router.get("/", getUsers);
 router.post("/", saveUser);
+router.post("/login", login);
 /* router.delete("/:id"); */
 
 export default router;
